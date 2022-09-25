288852eb81ec98d9c9473b13f7c06a645f6c293e
Matias <55811800+LedesmaMatias@users.noreply.github.com>
2022-09-25 20:39:52 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function abrirMapa() {
+  window.open("https://www.google.com/maps/place/Club+de+Regatas+América/@-34.4177773,-58.5799686,15z/data=!4m5!3m4!1s0x0:0xb274dfda2b8c20d9!8m2!3d-34.4177876!4d-58.5799406", "_blank");
+}
+
+function abrirConfirmacion() {
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLSer_tb8MugskMCP0kQP95NElMsah7OM3j291D2TZsjdhss_PQ/viewform", "_blank");
+}
